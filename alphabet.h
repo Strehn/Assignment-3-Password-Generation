16860acd0359ffaa0109278d8b991822eebac467
Sydney Petrehn <48782717+Strehn@users.noreply.github.com>
2025-04-06 15:23:58 -0600

    Add files via upload

--- alphabet.h
+++
@@ -1,7 +0,0 @@
-#ifndef ALPHABET_H
-#define ALPHABET_H
-
-void get_alphabet_union(const char *flags, const char *user_alphabet, char *available_chars);
-void validate_alphabet(const char *alphabet);
-
-#endif
--- pw_generator.h
+++
@@ -1,6 +0,0 @@
-#ifndef PW_GENERATOR_H
-#define PW_GENERATOR_H
-
-void generate_password(char *password, int length, const char *available_chars);
-
-#endif
